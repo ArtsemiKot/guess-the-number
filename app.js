d0f2623ea65f090bde4d736853ca4a2f1b3f2637
ArtsemiKot <kot_artemiy@mail.ru>
2023-09-05 10:40:54 +0300

    guess-the-number

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-alert('Добро пожаловать в игру угадай число')
+// Программа генерирует случайное число в диапазоне от 1 до 100 и просит пользователя угадать это число. 
+// Если догадка пользователя больше случайного числа, то программа должна вывести сообщение 
+// 'Слишком много, попробуйте еще раз'. Если догадка меньше случайного числа, то программа должна вывести сообщение 
+// 'Слишком мало, попробуйте еще раз'. Если пользователь угадывает число, то программа должна поздравить 
+// его и вывести сообщение 'Вы угадали, поздравляем!'.
 
-const n = Math.floor(Math.random() * 100);
-
-let count = 0;
-function searchBinary(n) {
-    let start = 0;
-    let end = 100;
-    for (let i = start; i <= end; i++) {
-        let average = Math.floor(prompt('Введите целое число'));
-        count++;
-        if (average == 0 || average > 101 || isNaN(average)) {
-            alert('Введите число и число должно быть больше 0 и меньше 101')
-            break;
-        }
-        if (n == average) {
-            alert(`Вы угадали число!`);
-            alert(`Попыток ${count}`)
-            break
-        } else if (average > n) {
-            end = average
-            alert(`Попробуйте число меньше. Введите число от ${start} до ${end}`);
-        } else if (average < n) {
-            start = average
-            alert(`Попробуйте число больше. Введите число от ${start} до ${end}`);
+class SearchBinary {
+    dosearchBinary(n) {
+        alert('Добро пожаловать в игру угадай число');
+        alert('Угадай число от 1 до 100');
+        let count = 0;
+        let start = 0;
+        let end = 100;
+        for (let i = start; i <= end; i++) {
+            let average = Math.floor(prompt('Введите целое число'));
+            count++;
+            if (average == 0 || average > 101 || isNaN(average)) {
+                alert('Введите число и число должно быть больше 0 и меньше 101')
+                break;
+            }
+            if (n == average) {
+                alert(`Вы угадали число!`);
+                alert(`Попыток ${count}`)
+                break;
+            } else if (average > n) {
+                end = average;
+                alert(`Вы ввели число ${average}. Попробуйте число меньше. Введите число от ${start} до ${end}`);
+            } else if (average < n) {
+                start = average;
+                alert(`Вы ввели число ${average}. Попробуйте число больше. Введите число от ${start} до ${end}`);
+            }
         }
     }
 }
-
-let result = searchBinary(n);
-console.log(result)
+const n = Math.floor(Math.random() * 100);
+const searchBinary = new SearchBinary();
+const result = searchBinary.dosearchBinary(n);
+console.log(result);
